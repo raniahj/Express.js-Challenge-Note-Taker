9182a80030692d79a03fa10f2b6e54bca45b3272
raniahj <raniahj06@gmail.com>
2022-01-27 13:17:38 -0500

    server and json

---
+++ server.js
@@ -0,0 +1,17 @@
+// initiate server
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
