c2bd628b1e0f3afca48aecf965b066f072b86cee
raniahj <raniahj06@gmail.com>
2022-03-17 22:38:13 -0400

    add gitignore

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-// const { note } = require('../db/db.json');
 const UUID = require("uuid");
 const fs = require('fs');
 const path = require('path');
@@ -15,17 +14,10 @@ router.get("/notes", (req, res) => {
 });
 
 router.post("/notes", (req, res) => {
-    // const { newNote } = req;
     req.body.id = UUID.v1();
-    // console.log(newNote);
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         let oldData = JSON.parse(data);
         oldData.push(req.body);
-        // const oldData=JSON.parse(data);
-        // // add id to req.body, another key value pair
-        // const newNote=req.body;
-        // const newData=[newNote, ...oldData];
-        // res.send(newData);
     fs.writeFile("./db/db.json", JSON.stringify(oldData), (err) => {
         if (err) {
             res.status(500).send(err);
@@ -36,12 +28,10 @@ router.post("/notes", (req, res) => {
 });
 
 router.delete("/notes/:id", (req, res) => {
-    // read db.json, filter and write db.json
     const { id } = req.params;
     let doNotDel;
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         const oldData = JSON.parse(data);
-        // oldData = oldData.filter(note => note.id !== req.params.id);
         if (err) {
             res.status(500).send(err);
         }
@@ -55,7 +45,6 @@ router.delete("/notes/:id", (req, res) => {
             }
         });
         res.send(doNotDel)
-        // res.json("Success!");
     });  
 });
 module.exports = router;
